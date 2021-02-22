98763b0ad581db14b9d7d2f959e79bd8c08a6a53
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-02-22 12:07:41 +0800

    Created Collision files

---
+++ Don't Peek/Collision.h
@@ -0,0 +1,22 @@
+/* Start Header ************************************************************************/
+/*!
+\file Collision.h
+\team name Don't Peak
+\software name I don't want to do homework
+\authors
+Tan Wei Ling Felicia	weilingfelicia.tan@digipen.edu
+Margaret Teo Boon See	Teo.b@digipen.edu
+Loh Yun Yi Tessa	tessa.loh@digipen.edu
+Tan Jiajia, Amelia	t.jiajiaamelia@digipen.edu
+
+\date 22/01/2021
+\brief <give a brief description of this file>
+
+
+Copyright (C) 20xx DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+#pragma once
