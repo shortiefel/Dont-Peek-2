85e0f842a0028de22649cc1a4463f9c15235ed44
Felicia <snowpaw2343@gmail.com>
2021-02-22 12:50:53 +0800

    test

---
+++ Don't Peek/src/Main.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma once
+
+#ifndef DONTPEEK_MAIN_H_
+#define DONTPEEK_MAIN_H_
+
+//----------GLOBALS---------
+
+extern float g_dt; //delta time
+
+//----------INPUTS----------
+
+#include <AEEngine.h>
+
+
+
+#endif // !DONTPEEK_MAIN_H_
