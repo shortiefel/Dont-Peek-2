4594d96b229362eca60b760e722d1ed98d62be2f
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-02-28 14:24:37 +0800

    Update GameState_DontPeek.cpp
    Updates some collision parts

--- Don't Peek/src/GameState_DontPeek.cpp
+++ Don't Peek/src/GameState_DontPeek.cpp
@@ -1,10 +1,14 @@
 #include "Main.h"
+#include "Collision.h"
 
 /******************************************************************************/
 /*!
 	DEFINES
 */
 /******************************************************************************/
+
+const unsigned int	GAME_OBJ_NUM_MAX = 32;			//The total number of different objects (Shapes)
+const unsigned int	GAME_OBJ_INST_NUM_MAX = 2048;			//The total number of different game object instances
 
 // -----------------------------------------------------------------------------
 // object flag definition
@@ -16,7 +20,14 @@ const unsigned long FLAG_ACTIVE = 0x00000001;
 	STRUCTS AND CLASS DEFINITIONS
 */
 /******************************************************************************/
+struct GameObjInst
+{
+	AABB boundingBox;// object bouding box that encapsulates the object
+	AEMtx33				transform;	// object transformation matrix: Each frame, 
+	float				scale;		// scaling value of the object instance
+	AEVec2				posCurr;	// object current position
 
+};
 
 
 
@@ -26,7 +37,8 @@ const unsigned long FLAG_ACTIVE = 0x00000001;
 	STATIC VARIABLES
 */
 /******************************************************************************/
-
+static GameObjInst			sGameObjInstList[GAME_OBJ_INST_NUM_MAX];	// Each element in this array represents a unique game object instance (sprite)
+static unsigned long		sGameObjInstNum;
 
 
 
@@ -60,7 +72,16 @@ void GameStateDontPeekInit(void)
 /******************************************************************************/
 void GameStateDontPeekUpdate(void)
 {
+	for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
+	{
+		GameObjInst* pInst = sGameObjInstList + i;
 
+		pInst->boundingBox.min.x = pInst->posCurr.x - pInst->scale * 0.5f;
+		pInst->boundingBox.min.y = pInst->posCurr.y - pInst->scale * 0.5f;
+
+		pInst->boundingBox.max.x = pInst->posCurr.x + pInst->scale * 0.5f;
+		pInst->boundingBox.max.y = pInst->posCurr.y + pInst->scale * 0.5f;
+	}
 }
 
 /******************************************************************************/
