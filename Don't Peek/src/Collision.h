fd06425c754b545d48ce940935636311b7e3e06f
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-02-28 13:39:36 +0800

    Update Collision.h

--- Don't Peek/src/Collision.h
+++ Don't Peek/src/Collision.h
@@ -22,6 +22,13 @@ Technology is prohibited.
 #pragma once
 
 #include <AEEngine.h>
-#include <stdio.h>
-#include <iostream>
+
+struct AABB
+{
+	AEVec2	min;
+	AEVec2	max;
+};
+
+bool CollisionIntersection_RectRect(const AABB& aabb1, const AEVec2& vel1,
+	const AABB& aabb2, const AEVec2& vel2);
 
