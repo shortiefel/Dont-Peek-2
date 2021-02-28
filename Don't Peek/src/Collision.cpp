9571f8343d2f22f4746d1127cb9ab73914af64b8
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-02-28 13:39:40 +0800

    Update Collision.cpp

--- Don't Peek/src/Collision.cpp
+++ Don't Peek/src/Collision.cpp
@@ -20,5 +20,84 @@ Technology is prohibited.
 */
 /* End Header **************************************************************************/
 
-#include "main.h"
 #include "Collision.h"
+
+bool CollisionIntersection_RectRect(const AABB& aabb1, const AEVec2& vel1,
+	const AABB& aabb2, const AEVec2& vel2)
+{
+	/*
+	Step 1: Check for static collision detection between rectangles (before moving).
+				If the check returns no overlap you continue with the following next steps (dynamics).
+				Otherwise you return collision true
+	*/
+	if (aabb1.max.x < aabb2.min.x || aabb1.max.y < aabb2.min.y ||
+		aabb1.min.x > aabb2.max.x || aabb1.min.y > aabb2.max.y)
+	{
+	}
+	else
+		return 1;
+	/*
+	Step 2: Initialize and calculate the new velocity of Vb
+			tFirst = 0
+			tLast = dt
+	*/
+	float tfirst{ 0.0f };
+	float tLast{ g_dt };
+	/*
+	Step 3: Working with one dimension (x-axis).
+			if(Vb < 0)
+				case 1
+				case 4
+			if(Vb > 0)
+				case 2
+				case 3
+
+			case 5
+	*/
+	float velocity = vel2.x - vel1.x;
+	if (velocity < 0)
+	{
+		if (aabb1.min.x > aabb2.max.x)
+			return 0;
+		if (aabb1.max.x < aabb2.min.x)
+			tfirst = AEMax((aabb1.max.x - aabb2.min.x) / velocity, tfirst);
+		if (aabb1.min.x < aabb2.max.x)
+			tLast = AEMin((aabb1.min.x - aabb2.max.x) / velocity, tLast);
+	}
+
+	else if (velocity > 0)
+	{
+		if (aabb1.max.x < aabb2.min.x)
+			return 0;
+		if (aabb1.min.x > aabb2.max.x)
+			tfirst = AEMax((aabb1.min.x - aabb2.max.x) / velocity, tfirst);
+		if (aabb1.max.x > aabb2.min.x)
+			tLast = AEMin((aabb1.max.x - aabb2.min.x) / velocity, tLast);
+	}
+	//Step 4: Repeat step 3 on the y-axis
+	velocity = vel2.y - vel1.y;
+
+	if (velocity < 0)
+	{
+		if (aabb1.min.y > aabb2.max.y)
+			return 0;
+		if (aabb1.max.y < aabb2.min.y)
+			tfirst = AEMax((aabb1.max.y - aabb2.min.y) / velocity, tfirst);
+		if (aabb1.min.y < aabb2.max.y)
+			tLast = AEMin((aabb1.min.y - aabb2.max.y) / velocity, tLast);
+	}
+	else if (velocity > 0)
+	{
+		if (aabb1.max.y < aabb2.min.y)
+			return 0;
+		if (aabb1.min.y > aabb2.max.y)
+			tfirst = AEMax((aabb1.min.y - aabb2.max.y) / velocity, tfirst);
+		if (aabb1.max.y > aabb2.min.y)
+			tLast = AEMin((aabb1.max.y - aabb2.min.y) / velocity, tLast);
+	}
+	//Step 5: Otherwise the rectangles intersect
+	if (tfirst > tLast)
+		return 0;
+
+	return 1;
+}
