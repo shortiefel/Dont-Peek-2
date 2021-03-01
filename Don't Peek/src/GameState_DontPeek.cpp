85efe97621c75e280e5603ab508b3edf700288ec
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-03-01 15:49:24 +0800

    Updated Gamestate.h

--- Don't Peek/src/GameState_DontPeek.cpp
+++ Don't Peek/src/GameState_DontPeek.cpp
@@ -1,49 +1,12 @@
 #include "Main.h"
 #include "Collision.h"
 
+
 /******************************************************************************/
 /*!
 	DEFINES
 */
 /******************************************************************************/
-
-const unsigned int	GAME_OBJ_NUM_MAX = 32;			//The total number of different objects (Shapes)
-const unsigned int	GAME_OBJ_INST_NUM_MAX = 2048;			//The total number of different game object instances
-
-// -----------------------------------------------------------------------------
-// object flag definition
-
-const unsigned long FLAG_ACTIVE = 0x00000001;
-
-/******************************************************************************/
-/*!
-	STRUCTS AND CLASS DEFINITIONS
-*/
-/******************************************************************************/
-struct GameObjInst
-{
-	AABB boundingBox;// object bouding box that encapsulates the object
-	AEMtx33				transform;	// object transformation matrix: Each frame, 
-	float				scale;		// scaling value of the object instance
-	AEVec2				posCurr;	// object current position
-
-};
-
-
-
-
-/******************************************************************************/
-/*!
-	STATIC VARIABLES
-*/
-/******************************************************************************/
-static GameObjInst			sGameObjInstList[GAME_OBJ_INST_NUM_MAX];	// Each element in this array represents a unique game object instance (sprite)
-static unsigned long		sGameObjInstNum;
-
-
-
-
-
 
 /******************************************************************************/
 /*!
--- Don't Peek/src/GameState_DontPeek.h
+++ Don't Peek/src/GameState_DontPeek.h
@@ -3,6 +3,46 @@
 #ifndef GAME_STATE_PLAY_H
 #define	GAME_STATE_PLAY_H
 
+/******************************************************************************/
+/*!
+	DEFINES
+*/
+/******************************************************************************/
+
+const unsigned int	GAME_OBJ_NUM_MAX = 32;			//The total number of different objects (Shapes)
+const unsigned int	GAME_OBJ_INST_NUM_MAX = 2048;			//The total number of different game object instances
+
+// -----------------------------------------------------------------------------
+// object flag definition
+
+const unsigned long FLAG_ACTIVE = 0x00000001;
+
+/******************************************************************************/
+/*!
+	STRUCTS AND CLASS DEFINITIONS
+*/
+/******************************************************************************/
+struct GameObjInst
+{
+	AABB boundingBox;// object bouding box that encapsulates the object
+	AEMtx33				transform;	// object transformation matrix: Each frame, 
+	float				scale;		// scaling value of the object instance
+	AEVec2				posCurr;	// object current position
+
+};
+
+
+
+
+/******************************************************************************/
+/*!
+	STATIC VARIABLES
+*/
+/******************************************************************************/
+static GameObjInst			sGameObjInstList[GAME_OBJ_INST_NUM_MAX];	// Each element in this array represents a unique game object instance (sprite)
+static unsigned long		sGameObjInstNum;
+
+
 void GameStateDontPeekLoad(void);
 void GameStateDontPeekInit(void);
 void GameStateDontPeekUpdate(void);
