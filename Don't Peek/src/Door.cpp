cbdbda15204ed6f95529ba1fd29300f956fdaf1d
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-03-02 01:19:59 +0800

    .

---
+++ Don't Peek/src/Door.cpp
@@ -0,0 +1,38 @@
+/* Start Header ************************************************************************/
+/*!
+\file Door.cpp
+\team name Don't Peak
+\software name I don't want to do homework
+\authors
+Tan Wei Ling Felicia	weilingfelicia.tan@digipen.edu
+Margaret Teo Boon See	Teo.b@digipen.edu
+Loh Yun Yi Tessa	tessa.loh@digipen.edu
+Tan Jiajia, Amelia	t.jiajiaamelia@digipen.edu
+
+\date 22/01/2021
+\brief <give a brief description of this file>
+
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
+void LoadDoor()
+{
+
+}
+void InitDoor()
+{
+
+}
+void UpdateDoor()
+{
+
+}
+void ExitDoor()
+{
+
+}
