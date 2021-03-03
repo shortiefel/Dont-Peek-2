4902fe90b0d737841c04b75fb9443b13ab3e787b
NoobBabyMilo <72365522+NoobBabyMilo@users.noreply.github.com>
2021-03-04 00:20:48 +0800

    Update Player.cpp

--- Don't Peek/src/Player.cpp
+++ Don't Peek/src/Player.cpp
@@ -58,7 +58,7 @@ void Player::Player_Draw()
 	// Drawing object 2 - (first) - No tint
 	AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
 	// Set position for object 2
-	AEGfxSetPosition(100.0f, -60.0f);
+	AEGfxSetPosition(Position.x, Position.y);
 	// No tint
 	AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
 
